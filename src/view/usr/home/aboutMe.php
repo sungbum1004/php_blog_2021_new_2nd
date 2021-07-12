1a1591298fd55b698b15ef320f197d52ddb1bad2
sungbum1004 <tlsqkfka96@naver.com>
2021-07-12 11:17:38 +0900

    여백 수정하기

--- src/view/usr/home/aboutMe.php
+++ src/view/usr/home/aboutMe.php
@@ -1,5 +1,5 @@
 <?php
-$pageTitleIcon = '<i class="fas fa-list"></i>';
+$pageTitleIcon = '<i class="far fa-id-card"></i>';
 $pageTitle = "주인장소개";
 ?>
 <?php require_once __DIR__ . "/../head.php"; ?>
@@ -7,7 +7,9 @@ $pageTitle = "주인장소개";
 
 <section class="section-about-me">
   <div class="container mx-auto">
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro, quasi atque tempora beatae dicta odit sequi nostrum at assumenda quibusdam illum neque tenetur fuga facilis dolorum recusandae ad molestias temporibus.
+    <div class="con-pad">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro, quasi atque tempora beatae dicta odit sequi nostrum at assumenda quibusdam illum neque tenetur fuga facilis dolorum recusandae ad molestias temporibus.
+    </div>
   </div>
 </section>
 <?php require_once __DIR__ . "/../foot.php"; ?>
