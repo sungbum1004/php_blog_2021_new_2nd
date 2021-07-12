3a2b8790447b1719ee3ec8f1b170be061c5b9b61
sungbum1004 <tlsqkfka96@naver.com>
2021-07-12 09:46:10 +0900

    최신 원본소스 적용

---
+++ src/view/usr/foot.php
@@ -0,0 +1,13 @@
+    </main>
+    <footer class="bottom-bar h-20 text-white">
+      <div class="container mx-auto h-full flex items-center justify-center">
+        <a href="/" class="top-bar__logo flex items-center">
+          <span><i class="fas fa-seedling"></i></span>
+          <span class="ml-2 font-bold">JSB BLOG</span>
+        </a>
+        <span class="ml-2">All rights reserved</span>
+      </div>
+    </footer>
+  </div>
+</body>
+</html>
